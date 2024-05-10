73c37affacec5b8db5c07f85b242f01b6f0756ba
WalexLima <151396969+WalexLima@users.noreply.github.com>
2024-05-10 20:58:40 -0300

    create

--- String_py/main.py
+++ String_py/main.py
@@ -1,1 +1,9 @@
-cpf = input("Digite seu CPF: ")
+
+email = 'walex@hotmail.com'
+email_gmail = email.endswith('@hotmail.com')
+email_hotmail = email.endswith('@gmail.com')
+
+if email_gmail or email_hotmail in email:
+    print('Método 4°: E-mail é válido')
+else:
+    print('Método 4°: E-mail é inválido')
