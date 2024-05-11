60a17a91f7b49caf358fc064c035d88ac7122a8a
WalexLima <151396969+WalexLima@users.noreply.github.com>
2024-05-11 17:25:12 -0300

    create

--- String_py/main.py
+++
@@ -1,9 +0,0 @@
-
-email = 'walex@hotmail.com'
-email_gmail = email.endswith('@hotmail.com')
-email_hotmail = email.endswith('@gmail.com')
-
-if email_gmail or email_hotmail in email:
-    print('Método 4°: E-mail é válido')
-else:
-    print('Método 4°: E-mail é inválido')
